a46052f9d3eec6a042654475eb4e27322eb17c79
Marco Braak <mbraak@ridethepony.nl>
2011-04-14 09:51:11 +0200

    Story 12282963. Fixed bug with inserting an image in markdown editor.

--- fiber/static/fiber/js/fiber.markitup.js
+++ fiber/static/fiber/js/fiber.markitup.js
@@ -6,10 +6,15 @@ Fiber.enhance_textarea = function(textarea) {
 
 	var header = $textarea.parent('.markItUpContainer').find('.markItUpHeader');
 	header.find('li.select_page a').click(function() {
+		// Restore textarea selection.
+		$textarea.caret(caret);
+
 		var page_select_dialog = new Fiber.PageSelectDialog();
 		page_select_dialog.action_click = function() {
-			var selected_page_path = this.uiDialog.find('a.ui-state-active').attr('href');
+			// Restore textarea selection.
+			$textarea.caret(caret);
 
+			var selected_page_path = this.uiDialog.find('a.ui-state-active').attr('href');
 			$.markItUp({
 				target: $textarea,
 				openWith: '"',
@@ -22,10 +27,15 @@ Fiber.enhance_textarea = function(textarea) {
 	});
 
 	header.find('li.select_link_to_image a').click(function() {
-		var imageSelectDialog = new Fiber.ImageSelectDialog();
-		imageSelectDialog.actionClick = function() {
-			var selectedImagePath = $(this.uiDialog.find('tr.ui-state-highlight td')[0]).text();
+		// Save selection in textarea. This is necessary for ie.
+		var caret = $textarea.caret();
 
+		var image_select_dialog = new Fiber.ImageSelectDialog();
+		image_select_dialog.action_click = function() {
+			// Restore textarea selection.
+			$textarea.caret(caret);
+
+			var selectedImagePath = $(this.uiDialog.find('tr.ui-state-highlight td')[0]).text();
 			$.markItUp({
 				target: $textarea,
 				openWith: '"',
@@ -38,10 +48,15 @@ Fiber.enhance_textarea = function(textarea) {
 	});
 
 	header.find('li.select_image a').click(function() {
+		// Save selection in textarea. This is necessary for ie.
+		var caret = $textarea.caret();
+
 		var image_select_dialog = new Fiber.ImageSelectDialog();
 		image_select_dialog.action_click = function() {
-			var selected_image_path = $(this.uiDialog.find('tr.ui-state-highlight td')[0]).text();
+			// Restore textarea selection.
+			$textarea.caret(caret);
 
+			var selected_image_path = $(this.uiDialog.find('tr.ui-state-highlight td')[0]).text();
 			$.markItUp({
 				target: $textarea,
 				replaceWith: '!' + selected_image_path + '!',
@@ -53,11 +68,15 @@ Fiber.enhance_textarea = function(textarea) {
 	});
 
 	header.find('li.select_file a').click(function() {
+		// Save selection in textarea. This is necessary for ie.
+		var caret = $textarea.caret();
+
 		var file_select_dialog = new Fiber.FileSelectDialog();
-
 		file_select_dialog.action_click = function() {
-			var selected_file_path = $(this.uiDialog.find('tr.ui-state-highlight td')[0]).text();
+			// Restore textarea selection.
+			$textarea.caret(caret);
 
+			var selected_file_path = $(this.uiDialog.find('tr.ui-state-highlight td')[0]).text();
 			$.markItUp({
 				target: $textarea,
 				openWith: '"',
