36ffe6a602c36d65cfe320a35d0c3e78b49e867b
Bram Simons <info@simons-design.nl>
2011-05-13 13:15:01 +0200

    whitespace

--- fiber/utils/date.py
+++ fiber/utils/date.py
@@ -26,23 +26,23 @@ def friendly_datetime(date_time):
         if seconds_diff < 10:
             return unicode(_('just now'))
         if seconds_diff < 60:
-            return  _('%s seconds ago') % str(seconds_diff)
+            return _('%s seconds ago') % str(seconds_diff)
         if seconds_diff < 120:
             return unicode('a minute ago')
         if seconds_diff < 3600:
-            return  _('%s minutes ago') % str(seconds_diff / 60)
+            return _('%s minutes ago') % str(seconds_diff / 60)
         if seconds_diff < 7200:
             return unicode(_('an hour ago'))
         if seconds_diff < 86400:
-            return  _('%s hours ago') % str(seconds_diff / 3600)
+            return _('%s hours ago') % str(seconds_diff / 3600)
     if days_diff == 1:
         return unicode(_('yesterday'))
     if days_diff < 7:
-        return  _('%s days ago') % str(days_diff)
+        return _('%s days ago') % str(days_diff)
     if days_diff < 14:
         return unicode(_('a week ago'))
     if days_diff < 31:
-        return  _('%s weeks ago') % str(days_diff / 7)
+        return _('%s weeks ago') % str(days_diff / 7)
     if days_diff < 365:
-        return  _('%s months ago') % str(days_diff / 30)
+        return _('%s months ago') % str(days_diff / 30)
     return _('%s years ago') % str(days_diff / 365)
