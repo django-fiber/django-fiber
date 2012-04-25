1d62b4f725c6d80540294b1c1d0aafb80c483bea
Marko Tibold <marko@tibold.nl>
2012-04-26 00:43:13 +0200

    Use multipart for uploads. Source: https://github.com/valums/file-uploader/pull/219/files

--- fiber/static/fiber/js/file-uploader/fileuploader.js
+++ fiber/static/fiber/js/file-uploader/fileuploader.js
@@ -257,6 +257,8 @@ qq.FileUploaderBasic = function(o){
         button: null,
         multiple: true,
         maxConnections: 3,
+        multipart: false,
+        fieldName: 'qqfile',
         // validation        
         allowedExtensions: [],               
         sizeLimit: 0,   
@@ -324,6 +326,8 @@ qq.FileUploaderBasic.prototype = {
             debug: this._options.debug,
             action: this._options.action,         
             maxConnections: this._options.maxConnections,   
+            multipart: this._options.multipart,
+            fieldName: this._options.fieldName,
             onProgress: function(id, fileName, loaded, total){                
                 self._onProgress(id, fileName, loaded, total);
                 self._options.onProgress(id, fileName, loaded, total);                    
@@ -1192,16 +1196,29 @@ qq.extend(qq.UploadHandlerXhr.prototype, {
             }
         };
 
-        // build query string
         params = params || {};
-        params['qqfile'] = name;
-        var queryString = qq.obj2url(params, this._options.action);
+
+        if(self._options.multipart){
+          var queryString = this._options.action;
+          var data = new FormData();
+          for (var paramName in params){
+            var paramValue = params[paramName];
+            data.append(paramName, paramValue);
+          }
+          data.append(self._options.fieldName, file);
+        } else {
+          // build query string
+          params[self._options.fieldName] = name;
+          var queryString = qq.obj2url(params, this._options.action);
+          var data = file;
+        }
 
         xhr.open("POST", queryString, true);
         xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xhr.setRequestHeader("X-File-Name", encodeURIComponent(name));
-        xhr.setRequestHeader("Content-Type", "application/octet-stream");
-        xhr.send(file);
+        if(!self._options.multipart) xhr.setRequestHeader("Content-Type", "application/octet-stream");
+
+        xhr.send(data);
     },
     _onComplete: function(id, xhr){
         // the request was aborted/cancelled
