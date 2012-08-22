19e6e5584fb696a325579114e21b34a88d62815c
Marko Tibold <mtibold@leukeleu.nl>
2012-08-22 16:46:47 +0200

    Improve configuration so that build works automatically without modifying the path manually.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
