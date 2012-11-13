d13fe54b8d8d3e17d06b6170b01346445e523ec0
Marko Tibold <marko@tibold.nl>
2012-11-13 01:00:21 +0100

    Clean up.

--- fiber/rest_api/urls.py
+++ fiber/rest_api/urls.py
@@ -1,8 +1,3 @@
-"""
-The implementation of this module is based on the example from the Django Rest Framework docs available at
-http://django-rest-framework.readthedocs.org/.
-"""
-
 from django.conf.urls.defaults import patterns, url
 
 from . import views
@@ -11,11 +6,11 @@ urlpatterns = patterns('',
     (r'^$', views.api_root),
     url(r'^pages/$', views.PageList.as_view(), name='page-list'),
     url(r'^pages/(?P<pk>[^/]+)/$', views.PageDetail.as_view(), name='page-detail'),
-    url(r'^pages/(?P<pk>[^/]+)/move_page/$', views.MovePageView.as_view(), name='page-resource-instance-move'),
+    url(r'^pages/(?P<pk>[^/]+)/move_page/$', views.MovePageView.as_view(), name='page-move'),
     url(r'^pagetree/$', views.PageTree.as_view(), name='pagetree'),
     url(r'^page_content_items/$', views.PageContentItemList.as_view(), name='pagecontentitem-list'),
     url(r'^page_content_items/(?P<pk>[^/]+)/$', views.PageContentItemDetail.as_view(), name='pagecontentitem-detail'),
-    url(r'^page_content_items/(?P<pk>[^/]+)/move/$', views.MovePageContentItemView.as_view(), name='page-content-item-resource-instance-move'),
+    url(r'^page_content_items/(?P<pk>[^/]+)/move/$', views.MovePageContentItemView.as_view(), name='pagecontentitem-move'),
     url(r'^content_items/$', views.ContentItemList.as_view(), name='contentitem-list'),
     url(r'^content_items/(?P<pk>[^/]+)/$', views.ContentItemDetail.as_view(), name='contentitem-detail'),
     url(r'^images/$', views.ImageList.as_view(), name='image-list'),
