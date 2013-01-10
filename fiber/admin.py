256f8cf4b113e580f72e3269bf3f5ec447e37445
Niels van Dijk <nvandijk@leukeleu.nl>
2013-01-10 15:53:17 +0100

    added meta_description to Fiber front-end admin

--- fiber/admin.py
+++ fiber/admin.py
@@ -177,11 +177,13 @@ class FiberAdminPageAdmin(UserPermissionMixin, fiber_admin.MPTTModelAdmin):
             self.fieldsets = (
                 (None, {'fields': ('title', 'url', )}),
                 (_('Advanced options'), {'fields': ('redirect_page', 'show_in_menu', 'is_public', )}),
+                (_('SEO'), {'fields': ('meta_description', )}),
             )
         else:
             self.fieldsets = (
                 (None, {'fields': ('title', 'url', )}),
                 (_('Advanced options'), {'fields': ('template_name', 'redirect_page', 'show_in_menu', 'is_public', )}),
+                (_('SEO'), {'fields': ('meta_description', )}),
             )
 
     def save_model(self, request, obj, form, change):
