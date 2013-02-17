e3c560a3f0e16fbb22ff7110882a5ef27d1a0b25
pahaz <white.mathematician@gmail.com>
2013-02-17 18:51:38 +0600

    jquery.slugify.js isolate. (for noconflict)

--- fiber/static/fiber/js/jquery.slugify.js
+++ fiber/static/fiber/js/jquery.slugify.js
@@ -1,13 +1,15 @@
-jQuery.fn.slugify = function(obj) {
-	jQuery(this).data('obj', jQuery(obj));
-	jQuery(this).keyup(function() {
-		var obj = jQuery(this).data('obj');
-		var slug = jQuery(this).val().replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
-		jQuery(this).data('obj').val(slug);
-	});
-	jQuery(this).blur(function() {
-		var obj = jQuery(this).data('obj');
-		var slug = jQuery(this).val().replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
-		jQuery(this).data('obj').val(slug);
-	});
-};
+(function (jQuery) {
+    jQuery.fn.slugify = function (obj) {
+        jQuery(this).data('obj', jQuery(obj));
+        jQuery(this).keyup(function () {
+            var obj = jQuery(this).data('obj');
+            var slug = jQuery(this).val().replace(/\s+/g, '-').replace(/[^a-zA-Z0-9\-]/g, '').toLowerCase();
+            jQuery(this).data('obj').val(slug);
+        });
+        jQuery(this).blur(function () {
+            var obj = jQuery(this).data('obj');
+            var slug = jQuery(this).val().replace(/\s+/g, '-').replace(/[^a-zA-Z0-9\-]/g, '').toLowerCase();
+            jQuery(this).data('obj').val(slug);
+        });
+    };
+})(jQuery);
