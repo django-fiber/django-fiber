d310e41aaa9ca356da6403b8d50ea8a6bab77623
Jaco Bovenschen <jbovenschen@leukeleu.nl>
2014-10-29 16:37:19 +0100

    Upgraded django compressor to 1.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=[
         'Pillow>=2.2.1',
         'django-mptt>=0.6.1',
-        'django_compressor>=1.3',
+        'django_compressor>=1.4',
         'djangorestframework>=2.3.8',
         'easy-thumbnails>=1.4',
     ],
