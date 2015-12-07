c918f89901684c9e1be3d89ae5d6e17fe1667f03
Jaap Roes <jroes@leukeleu.nl>
2015-12-07 15:59:03 +0100

    Be optimistic. We will support Python 3.4

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Framework :: Django',
     ]
 )
