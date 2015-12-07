7b9fc1aac4c164bba682df2cbf540df606ce2b60
Jaap Roes <jroes@leukeleu.nl>
2015-12-07 15:58:30 +0100

    Print is a function in Python 3

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ version = __import__('fiber').__version__
 
 if sys.argv[-1] == 'publish':  # upload to pypi
     os.system("python setup.py register sdist bdist_egg bdist_wheel upload")
-    print "You probably want to also tag the version now:"
-    print "  git tag -a %s -m 'version %s'" % (version, version)
-    print "  git push --tags"
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
     sys.exit()
 
 setup(
