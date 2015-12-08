40271bd9bfc017caa9df9ec3593379da360f829b
Jaap Roes <jroes@leukeleu.nl>
2015-12-08 10:55:51 +0100

    Referring to views as strings is deprecated

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -3,14 +3,16 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
+from django.views.i18n import javascript_catalog
 
+from fiber.views import page
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^api/v2/', include('fiber.rest_api.urls')),
     url(r'^admin/fiber/', include('fiber.admin_urls')),
-    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('fiber',), }),
+    url(r'^jsi18n/$', javascript_catalog, {'packages': ('fiber',), }),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
@@ -20,5 +22,5 @@ if settings.DEBUG:
 
 urlpatterns += [
     url(r'^empty/$', lambda request: HttpResponse('<!doctype html><html><head></head><body></body></html>')),
-    url(r'', 'fiber.views.page'),
+    url(r'', page),
 ]
