c7f5a531dd902630338f4b1156aea57eb7f912d0
Vladyslav <vladka@webdevelop.pro>
2016-07-24 23:52:11 -0400

    add get_page and get_child_pages tags

--- fiber/templatetags/fiber_tags.py
+++ fiber/templatetags/fiber_tags.py
@@ -263,3 +263,20 @@ def can_edit(obj, user):
 @register.simple_tag
 def fiber_version():
     return fiber.__version__
+
+@register.simple_tag(takes_context=True)
+def get_page(context, url, name=''):
+    if name == '':
+        name = url
+
+    context[name] = Page.objects.get(url=url)
+    return ''
+
+
+@register.simple_tag(takes_context=True)
+def get_child_pages(context, url, name=''):
+    if name == '':
+        name = url
+
+    context[name] = Page.objects.filter(parent__url=url)
+    return ''
