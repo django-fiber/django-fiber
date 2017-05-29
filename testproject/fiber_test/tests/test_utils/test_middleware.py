a52f4ebf4cc3d1906ef1901d6c9bdf431131e487
Luke Plant <L.Plant.98@cantab.net>
2017-05-29 14:51:13 +0300

    Fix test for Python 3

--- testproject/fiber_test/tests/test_utils/test_middleware.py
+++ testproject/fiber_test/tests/test_utils/test_middleware.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import django
 from django.test import RequestFactory
 from django.http import HttpResponse
@@ -8,10 +10,11 @@ class TestNewStyleMiddlewareMixin(object):
 
     if django.VERSION >= (1, 10):
         def test_new_style(self):
+            payload = b'Test'
             def get_response(request):
-                return HttpResponse('Test')
+                return HttpResponse(payload)
             request = RequestFactory().get('/')
 
             callable_middleware = self.middleware_factory(get_response)
             self.assertEqual(callable_middleware(request).content,
-                             'Test')
+                             payload)
