92f9701a062a380a4939049878444911473913a5
cho-leukeleu <cho@leukeleu.nl>
2017-09-11 15:33:15 +0200

    Improve performance of ObfuscateEmailAddressMiddleware on pages with a lot of data.
    i.e. a page with a large base64 encoded image

--- fiber/middleware.py
+++ fiber/middleware.py
@@ -165,12 +165,14 @@ class ObfuscateEmailAddressMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if is_html(response) and hasattr(response, 'content'):  # Do not obfuscate non-html and streaming responses.
             # http://www.lampdocs.com/blog/2008/10/regular-expression-to-extract-all-e-mail-addresses-from-a-file-with-php/
-            email_pattern = re.compile(r'(mailto:)?[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*(\+[_a-zA-Z0-9-]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.(([0-9]{1,3})|([a-zA-Z]{2,3})|(aero|coop|info|museum|name))')
-            response.content = email_pattern.sub(self.encode_email, force_text(response.content))
+            email_pattern = re.compile(r'\b(?P<email>(mailto:)?[\w-]+(\.[\w-]+)*(\+[\w-]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.(([0-9]{1,3})|([a-zA-Z]+)))\b')
+            response.content = email_pattern.sub(self.replace_email, force_text(response.content))
         return response
 
-    def encode_email(self, matches):
-        encoded_char_list = []
-        for char in matches.group(0):
-            encoded_char_list.append(random.choice(['&#%d;' % ord(char), '&#x%x;' % ord(char)]))
-        return ''.join(encoded_char_list)
+    def replace_email(self, matches):
+        email = matches.group('email')
+        return matches.group().replace(email, self.encode_email(email))
+
+    def encode_email(self, email):
+        formats = '&#{:d};', '&#x{:x};'
+        return ''.join(random.choice(formats).format(ord(c)) for c in email)
