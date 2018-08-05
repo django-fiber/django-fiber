b72c885221913a8cf497d23a18dde335dd7b295c
Robert Quitt <robertquitt@users.noreply.github.com>
2018-08-05 01:49:35 -0700

    Replace deprecated _get_val_from_obj
    Replaced it with value_from_object

--- fiber/utils/json.py
+++ fiber/utils/json.py
@@ -87,7 +87,7 @@ class JSONField(models.TextField):
         return self.to_python(value)
 
     def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
+        value = self.value_from_object(obj)
         return self._get_json_value(value)
 
 try:
